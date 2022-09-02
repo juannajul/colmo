a8a30d7452d8a3419138145e964abf6238115646
juanNajul <jnajul@marnagarage.com>
2022-09-01 23:59:23 -0400

    changes on mediafiles in settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = (
 
 # Media files 
 
-MEDIA_URL = "/mediafiles/"
-MEDIA_ROOT = BASE_DIR/"mediafiles"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
